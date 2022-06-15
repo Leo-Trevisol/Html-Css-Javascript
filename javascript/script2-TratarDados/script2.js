e41c4f825959f8c0d5245659371b9526fb0b027e
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-06-15 10:03:58 -0300

    NODE

--- javascript/script2-TratarDados/script2.js
+++ javascript/script2-TratarDados/script2.js
@@ -3,11 +3,41 @@
 var name = prompt('Enter your name:')
 var age = prompt('Enter you age:')
 var grade = prompt('Enter your school grade:')
+var cash = prompt('Enter your cash:')
 
   var test = ('The student ' + name + ' with ' +  age + ' years old, and grade ' + grade)
   alert(test)
- document.writeln(`the student ${name} with ${age} years old and grade: ${grade}`)
- document.writeln(` ${name}, your name have ${name.lenght()} letters!`)
+ document.write(`the student ${name} with ${age} years old and grade: ${grade} <br>`)
+ document.write(`${name}, your name in uppercase: ${name.toUpperCase()} <br> `)
+ cash.Number(toFixed(2))
+ cash.Number(toFixed(2).replace('.', ','))
+ cash.toLocateString('pt-br',{style:'currency', currency: 'BRL'})
+ document.write(`your cash it's: ${cash}`)
+
+
+
+/* NODE ===
+ > var cash = 1500
+undefined
+> cash.toFixed(2)
+'1500.00'
+> cash.toFixed(2).replace('.',',')
+'1500,00'
+> cash = 1500.5
+1500.5
+> cash.toFixed(2)
+'1500.50'
+> cash.toFixed(2).replace('.',',')
+'1500,50'
+> cash.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL')
+cash.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL')
+                                                                ^
+
+Uncaught SyntaxError: Unexpected token ')'
+> cash.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+'R$ 1.500,50'
+
+*/
 
 
 
