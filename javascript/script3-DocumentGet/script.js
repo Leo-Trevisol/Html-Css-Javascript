3cbefeca1b55f27e930b2d035a602abff97ed483
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-06-15 18:37:11 -0300

    document

---
+++ javascript/script3-DocumentGet/script.js
@@ -0,0 +1,17 @@
+
+
+//PEGANDO O ELEMENTO PELO ID === 
+
+var ide = document.getElementById('aga')
+ide.style.background = 'green'
+
+//PEGANDO O ELEMENTO PELA TAG ===
+
+var teg = document.getElementsByTagName('p')[0]
+teg.style.background = 'white'
+
+//PEGANDO O ELEMENTO PELA CLASSE === 
+
+var para = document.getElementsByClassName('pe')[1]
+para.style.background = 'black'
+
