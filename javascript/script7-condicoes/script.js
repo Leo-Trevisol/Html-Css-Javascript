b55af3833d7a94d3e4de64e336811aee0ad7b25b
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-06-19 00:32:59 -0300

    Update script.js

--- javascript/script7-condicoes/script.js
+++ javascript/script7-condicoes/script.js
@@ -19,6 +19,7 @@ var horamundial = agora.getUTCDate()
 //EVENTOS DO BOTAO ===
 
 bt.addEventListener('click', definirmes)
+bt.addEventListener('click', definirhora)
 bt.addEventListener('click', clicar)
 
 //FUNCAO QUE ESCREVE O TEXTO ===
@@ -26,7 +27,7 @@ bt.addEventListener('click', clicar)
 function clicar(){
     
     time.innerHTML = `Voce esta no ano ${ano}, no mes ${mes}, no dia ${dia} <br>
-     e agora sao ${hora} horas, ${min} minutos e ${sec} segundos.`
+     e agora sao ${hora}, ${min} minutos e ${sec} segundos.`
 
 }
 
@@ -77,12 +78,92 @@ function definirmes(){
 
 function definirhora(){
 
-    switch(hora){
-        case 0:
+    if(hora <= 11){
+    for(var i=0; i==hora; i++ ){
+
+        if(hora == 0){
             hora = `Meia noite`
-            break
         }
+        if(hora == 1){
+            hora = `1 da manhã`
+        }
+        if(hora == 2){
+            hora = `2 da manhã`
+        }
+        if(hora == 3){
+            hora = `3 da manhã`
+        }
+        if(hora == 4){
+            hora = `4 da manhã`
+        }
+        if(hora == 5){
+            hora = `5 da manhã`
+        }
+        if(hora == 6){
+            hora = `6 da manhã`
+        }
+        if(hora == 7){
+            hora = `7 da manhã`
+        }
+        if(hora == 8){
+            hora = `8 da manhã`
+        }
+        if(hora == 9){
+            hora = `9 da manhã`
+        }
+        if(hora == 10){
+            hora = `10 da manhã`
+        }
+        if(hora == 11){
+            hora = `11 da manhã`
+        }
+
     }
+}
+
+    if(hora >= 12){
+        for(var i=0; i==hora; i++ ){
+
+            if(hora == 0){
+                hora = `Meio dia`
+            }
+            if(hora == 1){
+                hora = `1 da tarde`
+            }
+            if(hora == 2){
+                hora = `2 da tarde`
+            }
+            if(hora == 3){
+                hora = `3 da tarde`
+            }
+            if(hora == 4){
+                hora = `4 da tarde`
+            }
+            if(hora == 5){
+                hora = `5 da tarde`
+            }
+            if(hora == 6){
+                hora = `6 da tarde`
+            }
+            if(hora == 7){
+                hora = `7 da noite`
+            }
+            if(hora == 8){
+                hora = `8 da noite`
+            }
+            if(hora == 9){
+                hora = `9 da noite`
+            }
+            if(hora == 10){
+                hora = `10 da noite`
+            }
+            if(hora == 11){
+                hora = `11 da noite`
+            }
+
+    }
+    }
+}
 
 
 
