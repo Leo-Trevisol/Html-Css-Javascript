3d72a9c6a1e9961974169d7f745c67541cac23bb
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-10-02 23:59:32 -0300

    oned

---
+++ curso-tecnico/Dia8/script.js
@@ -0,0 +1,113 @@
+var pp = document.getElementById('img4')
+pp.addEventListener('mouseenter', audio1)
+pp.addEventListener('mouseout', audio2)
+
+var har = document.getElementById('harry')
+har.addEventListener('mouseenter', harry)
+
+var lui = document.getElementById('luis')
+lui.addEventListener('mouseenter', luis)
+
+var zay = document.getElementById('zayn')
+zay.addEventListener('mouseenter', zayn)
+
+var lia = document.getElementById('liam')
+lia.addEventListener('mouseenter', liam)
+
+var nai = document.getElementById('nial')
+nai.addEventListener('mouseenter', nial)
+
+var banda1 = document.getElementById('banda')
+banda1.addEventListener('mouseenter', banda)
+
+var inicio1 = document.getElementById('inicio')
+inicio1.addEventListener('mouseenter', inicio)
+
+var carreira1 = document.getElementById('carreira')
+carreira1.addEventListener('mouseenter', carreira)
+
+var tiro1 = document.getElementById('tiro')
+tiro1.addEventListener('mouseenter', tiro)
+
+var sucesso1 = document.getElementById('sucesso')
+sucesso1.addEventListener('mouseenter', sucesso)
+
+var fim1 = document.getElementById('fim')
+fim1.addEventListener('mouseenter', fim)
+
+
+function audio1(){
+    var a = document.getElementById('sad')
+        a.play()
+}
+
+function audio2(){
+    var a = document.getElementById('sad')
+    a.pause()
+   }
+
+   function harry(){
+    var a = document.getElementById('audioharry')
+    a.play()
+    
+   }
+
+   function luis(){
+    var a = document.getElementById('audioluis')
+    a.play()
+    
+   }
+
+   function zayn(){
+    var a = document.getElementById('audiozayn')
+    a.play()
+    
+   }
+
+   function liam(){
+    var a = document.getElementById('audioliam')
+    a.play()
+    
+   }
+
+   function nial(){
+    var a = document.getElementById('audionial')
+    a.play()
+    
+   }
+
+   function banda(){
+    var a = document.getElementById('audiobanda')
+    a.play()
+    
+   }
+
+   function inicio(){
+    var a = document.getElementById('audioinicio')
+    a.play()
+    
+   }
+
+   function carreira(){
+    var a = document.getElementById('audiocarreira')
+    a.play()
+    
+   }
+
+   function tiro(){
+    var a = document.getElementById('audiotiro')
+    a.play()
+    
+   }
+
+   function sucesso(){
+    var a = document.getElementById('audiosucesso')
+    a.play()
+    
+   }
+
+   function fim(){
+    var a = document.getElementById('audiofim')
+    a.play()
+    
+   }
