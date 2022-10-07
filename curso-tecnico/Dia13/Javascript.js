5039186f6b24797e324598ff36664ac930e49148
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-10-06 21:55:05 -0300

    prova

---
+++ curso-tecnico/Dia13/Javascript.js
@@ -0,0 +1,15 @@
+// var a = "continuar?";
+// if(confirm(a)){
+//     alert('Continua menóoooo')
+// }else{
+//     alert('Encerra menóoooo')
+// }
+
+var n1 = window.prompt('Quem é amigo?')
+
+
+document.write(`<h1>${n1} é amigo</h1>`)
+
+var origem = prompt("Informe seu pais de origem \n brazuca", "Brasil");
+
+alert(`Voce mora na ${origem}`)
