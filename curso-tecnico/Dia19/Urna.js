75e201eba7130f542c4af03585411687c70acf15
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-10-27 17:23:11 -0300

    att

--- curso-tecnico/Dia19/Urna.js
+++ curso-tecnico/Dia19/Urna.js
@@ -9,14 +9,20 @@ var candidato2 = 55
 
 var tamanho = new Array()
 
+
+
 function print(){
+
+    for(let i =0; i<listbt.length;i++){
+        listbt[i].addEventListener('click', alert(listbt[i].value))
+    }
 
 if(tamanho.length>1){
     alert('Apenas 2 digitos')
 }else{
     tamanho.push(1)
     text.value += '1'
-    imagem1.style.display = 'block'
+   // imagem1.style.display = 'block'
 }
 
 
